3ca586394404a02fcffee816f46023b19f0ede25
Kajal Chaudhari <57319926+Kajal-9130@users.noreply.github.com>
2020-11-19 17:08:12 +0530

    Add files via upload

---
+++ cv.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2020 at 12:37 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlogin_tb`
+--
+
+CREATE TABLE `adminlogin_tb` (
+  `a_login_id` int(11) NOT NULL,
+  `a_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `a_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `a_password` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `adminlogin_tb`
+--
+
+INSERT INTO `adminlogin_tb` (`a_login_id`, `a_name`, `a_email`, `a_password`) VALUES
+(1, 'Admin', 'admin@gmail.com', '123456');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
